2a1222a271bba6d36b9ee6a27ea53381ec4aaadb
Pardhu543 <pchakolthi@gmail.com>
2026-02-23 11:30:03 +0000

    complete backbone of the microservice architecture

--- product-service/src/main/java/com/hero/product_service/ProductServiceApplication.java
+++ product-service/src/main/java/com/hero/product_service/ProductServiceApplication.java
@@ -2,8 +2,12 @@ package com.hero.product_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication(scanBasePackages = "com.hero.product")
+@SpringBootApplication(scanBasePackages = "com.hero")
+@EnableJpaRepositories(basePackages = "com.hero.product.repository")
+@EntityScan(basePackages = "com.hero.product.entity")
 public class ProductServiceApplication {
 
 	public static void main(String[] args) {
